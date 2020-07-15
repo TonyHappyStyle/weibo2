c3129c547c1a532f6d9dbd5e27d776a74d3bc0ea
TonyHappyStyle <TonyHappyStyle@163.com>
2020-07-15 10:04:46 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已退出登录');
+        return redirect('login');
+    }
 }
